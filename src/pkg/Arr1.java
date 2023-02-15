5d776c617d26f8a584205a510489c0a46d690562
Popat <chavanpopat1990@gmail.com>
2023-02-15 23:12:24 +0530

    first

---
+++ src/pkg/Arr1.java
@@ -0,0 +1,17 @@
+package pkg;
+
+import java.util.Arrays;
+
+public class Arr1 {
+
+	public static void main(String[] args) {
+		
+		int[] a= {12,34,32,7,81,45};
+		System.out.println(Arrays.toString(a));
+		Arrays.sort(a);
+		System.out.println(Arrays.toString(a));
+		
+
+	}
+
+}
---
+++ src/pkg/Pract.java
@@ -0,0 +1,26 @@
+package pkg;
+
+public class Pract {
+
+	public static void main(String[] args) {
+
+		//		// print infinity 
+		
+//		for(;;)
+//		{
+//			System.out.println("Hello");
+//		}
+		
+//		for(int i=0;i<1;i--)
+//		{
+//			System.out.println("Hello");
+//		}
+		
+		for(int i=1;i>0;i++)
+		{
+			System.out.println("Hello");
+		}
+
+	}
+
+}
